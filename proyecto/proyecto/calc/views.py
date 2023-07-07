850495797cb0eaf768ec6e1103de69cdd4ade0bf
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-07 12:25:44 -0500

    cpmmit

--- proyecto/proyecto/calc/views.py
+++ proyecto/proyecto/calc/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'home.html',{'name':'andre'})
 def add(request):
-    val1 = request.GET['num1']
-    val2 = request.GET['num2']
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     res = val1 + val2
     return render(request,'result.html',{'result':res})
