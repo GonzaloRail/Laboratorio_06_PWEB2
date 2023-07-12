5552c4ff3a29616cfe5860ca0fc285874bf9d132
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-12 10:47:45 -0500

    imagenes

--- proyecto/proyecto/travello/views.py
+++ proyecto/proyecto/travello/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    dest1 = Destination()
+    dest1.name = "Peru"
+    dest1.desc = "Peru"
+    dest1.price = 700
+    dest1.img = 'destination_1.jpg'
+    dest2 = Destination()
+    dest2.name = "Peru"
+    dest2.desc = "Peru"
+    dest2.price = 700
+    dest2.img = 'destination_2.jpg'
+    dest3 = Destination()
+    dest3.name = "Peru"
+    dest3.desc = "Peru"
+    dest3.price = 700
+    dest3.img = 'destination_3.jpg'
+
+    dests = [dest1,dest2,dest3]
+
+
+    return render(request, "index.html",{'dests':dests})
