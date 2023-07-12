34e2c939191155395c917f5ed5c6cc4e9b7dfc03
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-12 11:46:57 -0500

    commit

--- proyecto/proyecto/travello/views.py
+++ proyecto/proyecto/travello/views.py
@@ -7,16 +7,21 @@ def index(request):
     dest1.desc = "Peru"
     dest1.price = 700
     dest1.img = 'destination_1.jpg'
+    dest1.offer = False
+
     dest2 = Destination()
     dest2.name = "Peru"
     dest2.desc = "Peru"
     dest2.price = 700
     dest2.img = 'destination_2.jpg'
+    dest2.offer = True
+
     dest3 = Destination()
     dest3.name = "Peru"
     dest3.desc = "Peru"
     dest3.price = 700
     dest3.img = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1,dest2,dest3]
 
